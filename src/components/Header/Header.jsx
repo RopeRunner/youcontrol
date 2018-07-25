3e68fbeae480f947f12c0b112422fb1cec065479
a@eqnx.cc <glitchwave13@gmail.com>
2018-07-25 14:57:09 +0300

    [+] core structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+/*
+ * Component for navigsating to whole Youcontrol service 
+ */
+
 const Header = _ => <header />;
 
 export default Header;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = _ => (
+  <section>
+    <div />
+    <div />
+    <div />
+    <div />
+  </section>
+);
+
+export default Navigation;
