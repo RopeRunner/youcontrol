d5b5cc3ccc0bb37a35156723f6caa873ad0d7dcf
PROgrammaker72 <o.o.diordiy@gmail.com>
2018-08-10 11:16:13 +0300

    [+]movable nodes to their parents

--- src/components/GraphModule/listeners/findPosition.js
+++ src/components/GraphModule/listeners/findPosition.js
@@ -9,8 +9,34 @@ const findPosition = (
   console.log(nodeId);
   if (!rebuildedData[nodeId].isClosed) {
     for (let key in rebuildedData[nodeId]) {
+      if (key in defaultNodeValues) continue;
+
+      console.log('personal keys: ' + key);
       if (
-        key in defaultNodeValues ||
+        rebuildedData[key].parentNode &&
+        rebuildedData[key].fx &&
+        rebuildedData[key].fy
+      ) {
+        const parentId = rebuildedData[key].parentNode;
+        const coords = {
+          x: rebuildedData[parentId].fx || rebuildedData[parentId].x,
+          y: rebuildedData[parentId].fy || rebuildedData[parentId].y
+        };
+
+        if (
+          coords.x &&
+          coords.y &&
+          (Math.abs(coords.x - rebuildedData[key].fx) > 300 ||
+            Math.abs(coords.y - rebuildedData[key].fy) > 300)
+        ) {
+          rebuildedData[key].fx = null;
+          rebuildedData[key].fy = null;
+        }
+
+        continue;
+      }
+
+      if (
         (rebuildedData[key].fx && rebuildedData[key].fy) ||
         (rebuildedData[key].x && rebuildedData[key].y)
       )
@@ -25,12 +51,12 @@ const findPosition = (
           rebuildedData[nodeId].fx || rebuildedData[nodeId].x;
         rebuildedData[key].y =
           rebuildedData[nodeId].fy || rebuildedData[nodeId].y;
-        console.log(
-          'not fixed position: ',
-          key,
-          rebuildedData[key].x,
-          rebuildedData[key].y
-        );
+        // console.log(
+        //   'not fixed position: ',
+        //   key,
+        //   rebuildedData[key].x,
+        //   rebuildedData[key].y
+        // );
       } else {
         const coords = moveToNewLocation(
           rebuildedData,
@@ -41,7 +67,29 @@ const findPosition = (
 
         rebuildedData[key].fx = coords.x;
         rebuildedData[key].fy = coords.y;
-        console.log('fixed position: ', key, coords.x, coords.y);
+        // console.log('fixed position: ', key, coords.x, coords.y);
+      }
+    }
+
+    if (
+      rebuildedData[nodeId].parentNode &&
+      rebuildedData[nodeId].fx &&
+      rebuildedData[nodeId].fy
+    ) {
+      const parentId = rebuildedData[nodeId].parentNode;
+      const coords = {
+        x: rebuildedData[parentId].fx || rebuildedData[parentId].x,
+        y: rebuildedData[parentId].fy || rebuildedData[parentId].y
+      };
+
+      if (
+        coords.x &&
+        coords.y &&
+        (Math.abs(coords.x - rebuildedData[nodeId].fx) > 300 ||
+          Math.abs(coords.y - rebuildedData[nodeId].fy) > 300)
+      ) {
+        rebuildedData[nodeId].fx = null;
+        rebuildedData[nodeId].fy = null;
       }
     }
   }
